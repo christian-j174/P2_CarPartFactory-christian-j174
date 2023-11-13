beff49b8fa713226019e192c7dd51eb432021f91
Christian J. Rodriguez <christian.rodriguez95@upr.edu>
2023-11-12 21:57:48 -0400

    implement getters and setters for Order class

--- src/main/Order.java
+++ src/main/Order.java
@@ -4,32 +4,37 @@ import interfaces.Map;
 
 public class Order {
    
+    int id;
+    String customerName;
+    Map<Integer, Integer> requestedParts;
+    boolean fulfilled;
+
     public Order(int id, String customerName, Map<Integer, Integer> requestedParts, boolean fulfilled) {
         
     }
     public int getId() {
-        
+        return this.id;
     }
     public void setId(int id) {
-        
+        this.id = id;
     }
     public boolean isFulfilled() {
-      
+      return fulfilled;
     }
     public void setFulfilled(boolean fulfilled) {
-        
+        this.fulfilled = fulfilled;
     }
     public Map<Integer, Integer> getRequestedParts() {
-       
+       return requestedParts;
     }
     public void setRequestedParts(Map<Integer, Integer> requestedParts) {
-       
+       this.requestedParts = requestedParts;
     }
     public String getCustomerName() {
-      
+      return this.customerName;
     }
     public void setCustomerName(String customerName) {
-        
+        this.customerName = customerName;
     }
     /**
      * Returns the order's information in the following format: {id} {customer name} {number of parts requested} {isFulfilled}
