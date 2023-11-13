e77938be766b473ce8a5d4a87c01ca23d8eeb872
Christian J. Rodriguez <christian.rodriguez95@upr.edu>
2023-11-12 22:52:13 -0400

    carPartFactory getters and setters

--- src/main/CarPartFactory.java
+++ src/main/CarPartFactory.java
@@ -1,63 +1,101 @@
 package main;
 
+import interfaces.Stack;
+
+import java.io.IOException;
+
+import interfaces.List;
+import interfaces.Map;
+
 public class CarPartFactory {
+
+    // Data 
+    String orderPath; 
+    String partsPath;
+
+    // Objects Models
+    List<PartMachine> machines;
+    Stack<CarPart> productionBin;
+    Map<Integer, CarPart> partCatalog;
+    Map<Integer, List<CarPart>> inventory;
+    List<Order> orders;
+    Map<Integer, Integer> defectives;
+
 
         
     public CarPartFactory(String orderPath, String partsPath) throws IOException {
                 
     }
+
+
     public List<PartMachine> getMachines() {
-       
+       return this.machines;
     }
     public void setMachines(List<PartMachine> machines) {
-        
+        this.machines = machines;
     }
+
+
     public Stack<CarPart> getProductionBin() {
-      
+      return this.productionBin;
     }
     public void setProductionBin(Stack<CarPart> production) {
-       
+       this.productionBin = production;
     }
+
+
     public Map<Integer, CarPart> getPartCatalog() {
-        
+        return partCatalog;
     }
     public void setPartCatalog(Map<Integer, CarPart> partCatalog) {
-        
+        this.partCatalog = partCatalog;
     }
+
+
     public Map<Integer, List<CarPart>> getInventory() {
-       
+       return inventory;
     }
     public void setInventory(Map<Integer, List<CarPart>> inventory) {
-        
+        this.inventory = inventory;
     }
+
+
     public List<Order> getOrders() {
-        
+        return orders;
     }
     public void setOrders(List<Order> orders) {
-        
+        this.orders = orders;
     }
+
+
     public Map<Integer, Integer> getDefectives() {
-        
+        return defectives;
     }
     public void setDefectives(Map<Integer, Integer> defectives) {
-        
+        this.defectives = defectives;
     }
 
     public void setupOrders(String path) throws IOException {
-       
+       // here you read and add from the csv
     }
+
+
     public void setupMachines(String path) throws IOException {
-       
+       // here you read and add from the csv
     }
     public void setupCatalog() {
         
     }
+
+
     public void setupInventory() {
         
     }
     public void storeInInventory() {
        
     }
+
+
     public void runFactory(int days, int minutes) {
         
     }
