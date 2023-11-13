86bb8aacd7bec543db64bfd5f0cdf27bee467734
Christian J. Rodriguez <christian.rodriguez95@upr.edu>
2023-11-12 23:57:47 -0400

    tmp tester

---
+++ src/main/coolMain.java
@@ -0,0 +1,23 @@
+
+package main;
+
+import java.io.IOException;
+
+public class coolMain { 
+    public static void main(String[] args) {
+        try {
+            CarPartFactory cpf = new CarPartFactory("input/orders.csv", "input/parts.csv");
+            System.out.println("Hola bby");
+            // System.out.println(cpf.getMachines().get(1));
+            // System.out.println(cpf.getMachines().get(6));
+            // System.out.println(cpf.getMachines().get(2));
+            // System.out.println(cpf.getPartCatalog().get(1).getName());
+            // cpf.runFactory(1, 30);
+            // cpf.generateReport();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
